fc8dad093ac8561da4d5f165a8b7b36d8edda20e
Anicodebekeh <102191636+Anicodebekeh@users.noreply.github.com>
2023-03-12 06:03:29 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const searchBox = document.querySelector('#searchBox')
+
+const googleIcon = document.querySelector('#googleIcon')
+
+const form= document.querySelector("#inputForm")
+
+googleIcon.addEventListener("click", function (){
+    searchBox.classList.toggle('active')
+})
+
+
